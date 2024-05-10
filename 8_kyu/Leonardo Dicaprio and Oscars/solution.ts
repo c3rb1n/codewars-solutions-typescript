c36334aa380e46ff98e6a92975f401b429e5f459
c3rb1n <ilya.prudnikov.97@gmail.com>
2024-05-10 08:13:59 +0300

    added solution for "Leonardo Dicaprio and Oscars" kata (8 kyu)

---
+++ 8_kyu/Leonardo Dicaprio and Oscars/solution.ts
@@ -0,0 +1,8 @@
+export const leo = (oscar: number): string =>
+    oscar === 88
+        ? 'Leo finally won the oscar! Leo is happy'
+        : oscar === 86
+        ? 'Not even for Wolf of wallstreet?!'
+        : oscar > 88
+        ? 'Leo got one already!'
+        : 'When will you give Leo an Oscar?';
